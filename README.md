5fb57bfd25a4a93728e263ecc8d0b5dcd760e6e7
Emma Maguire <Emma.Maguire@gcu.edu>
2020-10-06 17:30:02 -0700

    Cleaned up code

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # .bubbletools
-An extension for Chromium-based browsers providing helpful tools, shortcuts, and features to aid in and save time developing apps with Bubble.io  
+An extension for Chromium-based browsers providing helpful tools, shortcuts, and features to aid in and save time developing apps with Bubble.io 
+
+## Current Features
+* Debug mode toggle
+* Live/Test version toggle
 
 ![](https://github.com/jadedkitty/bubble-tools/blob/master/preview.png)
-
--- src/page_action/scripts.js
+++ src/page_action/scripts.js
@@ -1,113 +1,90 @@
+// Function to toggle a defined URL parameter on or off
 function urlParamToggler(paramName) {
-    chrome.tabs.query({
-            'active': true,
-            'windowId': chrome.windows.WINDOW_ID_CURRENT
-        },
-        function (tabs) {
-            var url = tabs[0].url;
-            if (url.includes("bubbleapps.io") == true) {
-                tabURL = new URL(url.toString());
-                console.log(tabURL);
-
-                let params = new URLSearchParams(tabURL.search.slice(1));
-                console.log(params.toString());
-
-                if (params.has(paramName) == false) {
-                    params.set(paramName, true);
-                    chrome.tabs.update(tabs.id, {
-                        url: tabURL + '?' + params
-                    });
-                    // window.close();
-                } else {
-                    chrome.tabs.update(tabs.id, {
-                        url: tabURL.origin + tabURL.pathname
-                    });
-                    // window.close();
-                }
-
-            } else {
-                alert("This ain't no Bubble.io app!");
-            }
+  chrome.tabs.query(
+    {
+      active: true,
+      windowId: chrome.windows.WINDOW_ID_CURRENT,
+    },
+    function (tabs) {
+      var url = tabs[0].url;
+      if (url.includes("bubbleapps.io") == true) {
+        tabURL = new URL(url.toString());
+        console.log(tabURL);
+        let params = new URLSearchParams(tabURL.search.slice(1));
+        console.log(params.toString());
+        if (params.has(paramName) == false) {
+          params.set(paramName, true);
+          chrome.tabs.update(tabs.id, {
+            url: tabURL + "?" + params,
+          });
+        } else {
+          chrome.tabs.update(tabs.id, {
+            url: tabURL.origin + tabURL.pathname,
+          });
         }
-    );
+      } else {
+        alert("This ain't no Bubble.io app!");
+      }
+    }
+  );
 }
 
+// Function to toggle between a defined version and live
 function urlVersionToggler(versionName) {
-    chrome.tabs.query({
-            'active': true,
-            'windowId': chrome.windows.WINDOW_ID_CURRENT
-        },
-        function (tabs) {
-            var url = tabs[0].url;
-            if (url.includes("bubbleapps.io") == true) {
-                tabURL = new URL(url.toString());
-                // console.log(tabURL);
-                let params = new URLSearchParams(tabURL.search.slice(1));
-                let version = tabURL.pathname.slice(1);
-                let newVersion = version.replace(versionName, "")
-                // console.log(version.replace("version-test",""));
-                // console.log(params.toString());
-                // console.log(tabURL.origin + newVersion + '?' + params)
-                if (params != null && params != "") {
-                    if (url.includes(versionName) == false) {
-                        // console.log(tabURL.origin + versionName + '?' + params)
-                        chrome.tabs.update(tabs.id, {
-                            url: tabURL.origin + '/' + versionName  + '/' + newVersion + '?' + params
-                        });
-                        // console.log('Updated to: ' + versionName)
-                    }
-                    else {
-                        // console.log(tabURL.origin + newVersion + '?' + params)
-                        chrome.tabs.update(tabs.id, {
-                            url: tabURL.origin + newVersion + '?' + params
-                        });
-                        // console.log('Updated to: ' + newVersion)
-                    }
-                } else {
-                    if (url.includes(versionName) == false) {
-                        // console.log(tabURL.origin + versionName)
-                        chrome.tabs.update(tabs.id, {
-                            url: tabURL.origin + '/' + versionName + '/' + newVersion
-                        });
-                        // console.log('Updated to: ' + versionName)
-                    }
-                    else {
-                        // console.log(tabURL.origin + newVersion )
-                        chrome.tabs.update(tabs.id, {
-                            url: tabURL.origin + newVersion
-                        });
-                        // console.log('Updated to: ' + newVersion)
-                    }
-                } 
-
-            } else {
-                alert("This ain't no Bubble.io app!");
-            }
+  chrome.tabs.query(
+    {
+      active: true,
+      windowId: chrome.windows.WINDOW_ID_CURRENT,
+    },
+    function (tabs) {
+      var url = tabs[0].url;
+      if (url.includes("bubbleapps.io") == true) {
+        tabURL = new URL(url.toString());
+        let params = new URLSearchParams(tabURL.search.slice(1));
+        let version = tabURL.pathname.slice(1);
+        let newVersion = version.replace(versionName, "");
+        if (params != null && params != "") {
+          if (url.includes(versionName) == false) {
+            chrome.tabs.update(tabs.id, {
+              url:
+                tabURL.origin +
+                "/" +
+                versionName +
+                "/" +
+                newVersion +
+                "?" +
+                params,
+            });
+          } else {
+            chrome.tabs.update(tabs.id, {
+              url: tabURL.origin + newVersion + "?" + params,
+            });
+          }
+        } else {
+          if (url.includes(versionName) == false) {
+            chrome.tabs.update(tabs.id, {
+              url: tabURL.origin + "/" + versionName + "/" + newVersion,
+            });
+          } else {
+            chrome.tabs.update(tabs.id, {
+              url: tabURL.origin + newVersion,
+            });
+          }
         }
-    );
+      } else {
+        alert("This ain't no Bubble.io app!");
+      }
+    }
+  );
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    var debugToggle = document.getElementById('debugtoggle');
-    // onClick's logic below:
-    debugToggle.addEventListener('click', function () {
-        urlParamToggler('debug_mode');
-    });
-    var versionToggle = document.getElementById('versiontoggle');
-    // onClick's logic below:
-    versionToggle.addEventListener('click', function () {
-        urlVersionToggler('version-test');
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var debugToggle = document.getElementById("debugtoggle");
+  debugToggle.addEventListener("click", function () {
+    urlParamToggler("debug_mode");
+  });
+  var versionToggle = document.getElementById("versiontoggle");
+  versionToggle.addEventListener("click", function () {
+    urlVersionToggler("version-test");
+  });
 });
-
-// var gear = document.getElementsByClassName('gear')[0];
-// var bubble = document.getElementById('bubble');
-// if (gearbubble) {
-//     gear.addEventListener('animationend', function (e) {
-//         gear.classList.remove('fast');
-//     });
-
-//     bubble.addEventListener('mouseover', function (e) {
-//         gear.classList.add('fast')
-//     })
-// }
